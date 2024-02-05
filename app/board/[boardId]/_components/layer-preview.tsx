c709f561c258c8e94e9e61d1c809bd939755cee6
Gopal Sasmal <gopalsasmal2000@gmail.com>
2024-02-05 13:55:44 +0530

    Added features [Text, Sticky Note, Ellipse]

--- app/board/[boardId]/_components/layer-preview.tsx
+++ app/board/[boardId]/_components/layer-preview.tsx
@@ -4,6 +4,11 @@ import { memo } from "react";
 import { useStorage } from "@/liveblocks.config";
 import { LayerType } from "@/types/canvas";
 import { Rectangle } from "./layers/rectangle";
+import { Ellipse } from "./layers/ellips";
+import { Text } from "./layers/text";
+import { Note } from "./layers/note";
+import { Path } from "./layers/path";
+import { colorToCss } from "@/lib/utils";
 
 interface LayerPreviewProps {
   id: string;
@@ -17,6 +22,45 @@ export const LayerPreview = memo(
     if (!layer) return null;
 
     switch (layer.type) {
+      case LayerType.Note:
+        return (
+          <Note
+            id={id}
+            layer={layer}
+            onPointerDown={onLayerPointerDown}
+            selectionColor={selectionColor}
+          />
+        );
+      case LayerType.Text:
+        return (
+          <Text
+            id={id}
+            layer={layer}
+            onPointerDown={onLayerPointerDown}
+            selectionColor={selectionColor}
+          />
+        );
+      case LayerType.Path:
+        return (
+          <Path
+            key={id}
+            points={layer.points}
+            onPointerDown={(e) => onLayerPointerDown(e, id)}
+            x={layer.x}
+            y={layer.y}
+            fill={layer.fill ? colorToCss(layer.fill) : "#000"}
+            stroke={selectionColor}
+          />
+        );
+      case LayerType.Ellipse:
+        return (
+          <Ellipse
+            id={id}
+            layer={layer}
+            onPointerDown={onLayerPointerDown}
+            selectionColor={selectionColor}
+          />
+        );
       case LayerType.Rectangle:
         return (
           <Rectangle
@@ -27,7 +71,7 @@ export const LayerPreview = memo(
           />
         );
       default:
-        console.log("unknow layer type");
+        console.warn("unknow layer type");
         return null;
     }
   }
---
+++ app/board/[boardId]/_components/layers/ellips.tsx
@@ -0,0 +1,33 @@
+import { colorToCss } from "@/lib/utils";
+import { EllipseLayer } from "@/types/canvas";
+
+interface EllipseProps {
+  id: string;
+  layer: EllipseLayer;
+  onPointerDown: (e: React.PointerEvent, id: string) => void;
+  selectionColor?: string;
+}
+
+export const Ellipse = ({
+  id,
+  layer,
+  onPointerDown,
+  selectionColor,
+}: EllipseProps) => {
+  return (
+    <ellipse
+      className="drop-shadow-md"
+      onPointerDown={(e) => onPointerDown(e, id)}
+      style={{
+        transform: `translate(${layer.x}px, ${layer.y}px)`,
+      }}
+      cx={layer.width / 2}
+      cy={layer.height / 2}
+      rx={layer.width / 2}
+      ry={layer.width / 2}
+      fill={layer.fill ? colorToCss(layer.fill) : "#ccc"}
+      stroke={selectionColor || "transparent"}
+      strokeWidth={"1"}
+    />
+  );
+};
